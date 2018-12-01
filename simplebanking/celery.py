2d3543d5d2ead990c3a619a7543a33d73f64eed6
Keda87 <adiyatmubarak@gmail.com>
2018-12-01 11:05:07 +0700

    setup celery

---
+++ simplebanking/celery.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplebanking.settings')
+
+app = Celery('simplebanking', broker='amqp://admin:admin@queuing:5672')
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.conf.update(
+    task_serializer='json',
+    result_serializer='json',
+    enable_utc=True,
+)
+
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
--- simplebanking/settings.py
+++ simplebanking/settings.py
@@ -142,3 +142,4 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
+# ------------------------------------------------------------------------------
