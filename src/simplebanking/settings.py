31833f196c0a12b51b72bc093a8bb64a7f6267cc
Keda87 <adiyatmubarak@gmail.com>
2020-06-20 11:11:32 +0700

    Change default db settings and hasher for testing purpose

--- src/simplebanking/settings.py
+++ src/simplebanking/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import sys
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -95,6 +97,18 @@ DATABASES = {
         'CONN_MAX_AGE ': 60
     }
 }
+
+if 'test' in sys.argv:
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.MD5PasswordHasher',
+    ]
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
